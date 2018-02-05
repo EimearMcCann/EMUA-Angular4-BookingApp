aede884f7bc7b77712a3bd1a4a567a64e1f4b0b3
EimearMcCann <L00105764@student.lyit.ie>
2018-02-05 15:00:07 +0000

    httpclientmoduleadded

--- emuaApp1/src/app/app.module.ts
+++ emuaApp1/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { Http } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +14,10 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    Http
+
   ],
   providers: [],
   bootstrap: [AppComponent]
