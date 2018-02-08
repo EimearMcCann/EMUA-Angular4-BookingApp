675f763c9ce9ebaabb4083b8d5900625ec2426da
EimearMcCann <L00105764@student.lyit.ie>
2018-02-08 13:02:48 +0000

    Updated GettingDataFromDatabase

---
+++ myproject/src/app/appointments/appointments.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-appointments',
+  templateUrl: './appointments.component.html',
+  styleUrls: ['./appointments.component.css']
+})
+export class AppointmentsComponent {
+
+
+  public items : Array<any> =[];
+  constructor(public http : HttpClient) {
+
+  }
+
+  ionViewWillEnter() : void{
+    this.load();
+  }
+  load() : void{
+    this.http
+
+    .get('http://localhost/retrieve-AppointmentAWS.php')
+    .subscribe((data : any) =>
+  {
+    console.dir(data);
+    this.items = data;
+  },
+  (error : any) =>{
+    console.dir(error);
+  });
+  }
+}
+  
+
+
+
