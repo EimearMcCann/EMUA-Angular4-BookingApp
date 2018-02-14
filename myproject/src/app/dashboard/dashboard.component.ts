3b68c019a0c983705465e824a4ec49ce739a74d8
EimearMcCann <L00105764@student.lyit.ie>
2018-02-14 14:54:06 +0000

    viewClientsandAppointmentsbuttonMovedToDashboard

--- myproject/src/app/dashboard/dashboard.component.ts
+++ myproject/src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { UserService } from '../user.service';
-
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
+  public items : Array<any>=[];
+  Client_Fname:string='';
 
-   
 
-  constructor(private user: UserService) { }
-
-  ngOnInit() {
-  }
-
+  constructor(private httpClient:HttpClient){}
+  
+ionViewWillEnter() : void{
+  this.load
 }
+
+  load():void{
+    this.httpClient.get('http://localhost/retrieve-dataAWS.php')
+    .subscribe(
+      (data:any[])=>{
+        console.log(data);
+        this.items= data;
+      },
+      (error:any) =>{
+        console.dir(error);
+      });
+
+  }
+ }
+
