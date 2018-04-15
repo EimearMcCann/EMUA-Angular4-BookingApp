3f5f313ebb59ec56a1936f1cf2592b8b52eb0e5e
Eimear McCann <L00105764@student.lyit.ie>
2018-04-15 11:57:55 +0100

    updated1

--- myproject/src/app/dashboard/dashboard.component.ts
+++ myproject/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component} from '@angular/core';
-import { UserService } from '../user.service';
 import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-dashboard',
